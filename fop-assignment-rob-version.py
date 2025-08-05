1f2ae0bae9ecfb9b43a3bc240a5cd685485feed1
Rob Frawley 2nd <rmf@src.run>
2025-08-04 22:50:04 -0400

    Update fop-assignment-rob-version.py
    cleaned up terminal output formatting

--- fop-assignment-rob-version.py
+++ fop-assignment-rob-version.py
@@ -16,10 +16,6 @@ class Fighter:
         if not self.is_alive():
             return
 
-        print("=========================") 
-        print(f"{self.name.upper()}'S TURN!")
-        print("=========================")
-
         damage = random.randint(int(self.pwr / 2), self.pwr) - opponent.defense
 
         if damage < 0:
@@ -27,9 +23,7 @@ class Fighter:
 
         opponent.hp -= damage
 
-        print(f"{self.name} attacks {opponent.name} for {damage} damage!")
-        self.write_stats()
-        opponent.write_stats()
+        print(f"!! {self.name} attacks {opponent.name} for {damage} damage!")
 
     def can_outrun(self, opponent):
         return random.randint(int(self.speed / 2), int(self.speed * 2)) > random.randint(int(opponent.speed / 2), int(opponent.speed * 2))
@@ -38,7 +32,7 @@ class Fighter:
         return self.hp > 0
     
     def write_stats(self):
-        print(f"Name: {self.name} / HP: {self.hp} / Power: {self.pwr} / Defense: {self.defense} / Speed: {self.speed}")
+        print(f"-- Player Stats: [power: {self.pwr}, defense: {self.defense}, speed: {self.speed}, hp: {self.hp}, state: {'alive' if self.is_alive() else 'dead'}, name: {self.name}]")
 	
 	
 class Arena:
@@ -47,21 +41,23 @@ class Arena:
         self.contestant2 = contestant2
 		
     def run_match(self):
-        print("===========ARENA============")
-        print("============================")
-
         while self.contestant1.is_alive() and self.contestant2.is_alive():
+            print("## RUNNING MATCH TURN...")
+
             if self.contestant1.can_outrun(self.contestant2):
-                print(f"# {self.contestant1.name} can outrun {self.contestant2.name} ({self.contestant1.name} attacks first).")
+                print(f"-- {self.contestant1.name} can outrun {self.contestant2.name} ({self.contestant1.name} attacks first).")
                 self.contestant1.attack(self.contestant2)
                 self.contestant2.attack(self.contestant1)
             else:
-                print(f"# {self.contestant2.name} can outrun {self.contestant1.name} ({self.contestant2.name} attacks first).")
+                print(f"-- {self.contestant2.name} can outrun {self.contestant1.name} ({self.contestant2.name} attacks first).")
                 self.contestant2.attack(self.contestant1)
                 self.contestant1.attack(self.contestant2)
 
-        print(f"{self.contestant1.name} is {'alive' if self.contestant1.is_alive() else 'dead'}!")
-        print(f"{self.contestant2.name} is {'alive' if self.contestant2.is_alive() else 'dead'}!")
+            self.contestant1.write_stats()
+            self.contestant2.write_stats()
+
+        print(f"++ {self.contestant1.name} is {'alive' if self.contestant1.is_alive() else 'dead'}!")
+        print(f"++ {self.contestant2.name} is {'alive' if self.contestant2.is_alive() else 'dead'}!")
 
 
 def main():
