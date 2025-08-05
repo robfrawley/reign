12b65445b23736b3e0525d1022e115eed87833c6
Rob Frawley 2nd <rmf@src.run>
2025-08-05 17:09:39 -0400

    Update fop-assignment-rob-version.py
    change healing behavior per attack to self, add healing properties to stats output

--- fop-assignment-rob-version.py
+++ fop-assignment-rob-version.py
@@ -5,42 +5,36 @@
 import random
 
 class Fighter:
-    def __init__(self, name = "", health = 250, damage = 100, defense = 25, speed = 100, heal_ability = 50, heal_chance = 0.75):
+    def __init__(self, name = "", health = 250, damage = 100, defense = 50, speed = 100, heal_amount = 50, heal_chance = 0.5):
         self.name = name
         self.health_max = health
         self.health = health
         self.damage = damage
         self.defense = defense
         self.speed = speed
-        self.heal_ability = heal_ability
+        self.heal_amount = heal_amount
         self.heal_chance = heal_chance
 
     def attack(self, opponent):
         if not self.is_alive():
             return
 
-        damage = Fighter.calculate_stat_from_range(self.damage) - opponent.defense
-
-        if damage < 0:
-            damage = 0
+        damage = max(0, Fighter.calculate_stat_from_range(self.damage) - opponent.defense)
 
         opponent.health -= damage
 
         print(f"!! {self.name} attacks {opponent.name} for {damage} damage!")
 
-        opponent.heal()
+        self.heal()
 
     def heal(self):
         if random.random() > self.heal_chance:
-            print(f"-- {self.name} could not heal.")
+            print(f"++ {self.name} could not heal!")
             return
 
-        heal_amount = Fighter.calculate_stat_from_range(self.heal_ability)
+        heal_amount = Fighter.calculate_stat_from_range(self.heal_amount)
 
-        self.health += heal_amount
-
-        if self.health > self.health_max:
-            self.health = self.health_max
+        self.health = min(self.health + heal_amount, self.health_max)
 
         print(f"++ {self.name} heals for {heal_amount} health!")
 
@@ -51,7 +45,7 @@ class Fighter:
         return self.health > 0
     
     def __str__(self):
-        return f"-- Fighter (name={self.name}, health={self.health}, damage={self.damage}, defense={self.defense}, speed={self.speed}, state={"alive" if self.is_alive() else "dead"})"
+        return f"-- Fighter (name={self.name}, health={self.health}/{self.health_max}, damage={self.damage}, defense={self.defense}, speed={self.speed}, heal_amount={self.heal_amount}, heal_chance={self.heal_chance}, state={"alive" if self.is_alive() else "dead"})"
 
     @staticmethod
     def calculate_stat_from_range(stat, low_bounds_divisor = 2, high_bounds_multiplier = 1.5):
